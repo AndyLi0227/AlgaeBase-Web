9c519d03b962b78ad90f48b497ddc5483a47c305
AndyLi0227 <likwoksing227@gmail.com>
2021-12-23 21:26:14 +0800

    Andy

--- Website/AlgaeBase_web_3/user/urls.py
+++ Website/AlgaeBase_web_3/user/urls.py
@@ -20,5 +20,5 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-         path('',views.index,name='index'),
+         path('index/',views.index,name='index'),
 ]
